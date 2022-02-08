f9fb533852f17b3d25235b46176a08614e589a67
l3miage-franceto <tom.frances97@yahoo.fr>
2022-02-08 20:25:31 -0100

    début GradeTest

--- src/test/java/fr/uga/im2ag/l3/miage/db/repository/GradeTest.java
+++ src/test/java/fr/uga/im2ag/l3/miage/db/repository/GradeTest.java
@@ -2,12 +2,14 @@ package fr.uga.im2ag.l3.miage.db.repository;
 
 import fr.uga.im2ag.l3.miage.db.repository.api.GradeRepository;
 import fr.uga.im2ag.l3.miage.db.repository.api.SubjectRepository;
+import lombok.extern.java.Log;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@Log
 class GradeTest extends Base {
 
     GradeRepository gradeRepository;
@@ -37,7 +39,6 @@ class GradeTest extends Base {
 
         var pGrade = gradeRepository.findById(grade.getId());
         assertThat(pGrade).isNotNull().isNotSameAs(grade);
-        System.out.println(String.format("Assertion grade id = %d, subject = %s", pGrade.getId(), pGrade.getSubject().getName()));
         assertThat(pGrade.getId()).isEqualTo(grade.getId());
         assertThat(pGrade.getSubject().getName()).isEqualTo(grade.getSubject().getName());
     }
@@ -45,6 +46,20 @@ class GradeTest extends Base {
     @Test
     void shouldFailUpgradeGrade() {
         // TODO, ici tester que la mise à jour n'a pas eu lieu.
+        final var subject = Fixtures.createSubject();
+        final var grade = Fixtures.createGrade(subject);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(subject);
+        gradeRepository.save(grade);
+        entityManager.getTransaction().commit();
+        entityManager.detach(grade);
+
+        var pGrade = gradeRepository.findById(grade.getId());
+        assertThat(pGrade).isNotNull().isNotSameAs(grade);
+        pGrade.setValue((float)12.673);
+        assertThat(pGrade.getValue()).isNotEqualTo(grade.getValue());
+        log.info(String.format("pGrade value : %f, grade value : %f",pGrade.getValue(), grade.getValue()));
     }
 
     @Test
